fd20c4bd7eb04133bf1516942d2a57da3758f585
ddorz <davedorzback@gmail.com>
2018-11-11 14:45:24 -0500

    Added static .css file

--- Weeble/Weeble/settings.py
+++ Weeble/Weeble/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -131,4 +132,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..\\Weeble\\var\www\static\\")
+
 STATIC_URL = '/static/'
+
+STATIC_PATH = os.path.join(BASE_DIR, '..\\Weeble\static')
+
+STATICFILES_DIRS = (
+   STATIC_PATH,
+)
